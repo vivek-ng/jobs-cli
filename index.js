4427f43a17024273bca8b32552a1ece9c6cc6f7f
vivek3894 <viveknagaraju@yahoo.in>
2018-07-18 19:34:21 +0530

    added code to push interview details to redis

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 #!/usr/bin/env node
 
-console.log('vivek');
+let program = require('commander');
+
+let redis = require("redis"),
+    client = redis.createClient();
+
+    client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+
+// client.set('company' , 'amazon',redis.print);
+// console.log('company',redis.print);
+// client.quit();
+
+
+// function addCompany(){
+
+// }
+
+// client.hset("test" , "amazon" ,"bakwas", redis.print);
+// client.hset("test" , "google" ,"coding", redis.print);
+// client.hkeys("test", (err , replies) => {
+// 	replies.forEach((company) => {
+// 		console.log(company)
+// 	})
+// })
+
+program
+     .arguments('<company>')
+     .option('-s , --status [status]', 'status of the interview process')
+     .option('-d , --deadline [deadline]', 'deadline of the company')
+     .option('-n, --notes [notes]' , 'specific notes for the company')
+     .action(function(req, opts){
+     	console.log(req);
+     	console.log(opts.deadline); 
+     	let companyDetails = {};
+     	companyDetails.status = opts.status;
+     	companyDetails.deadline = opts.deadline;
+     	companyDetails.notes = opts.notes;
+
+     	client.hset("interviewDetails", req, JSON.stringify(companyDetails), redis.print);
+
+     })
+     .parse(process.argv)
